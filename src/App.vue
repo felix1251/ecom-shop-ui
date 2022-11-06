64daf231157df429c96986444630aa9456f44785
felix1251 <domhack123@gmail.com>
2022-11-06 23:50:10 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <a-layout :style="{height: '100vh'}">
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', lineHeight: '64px', display: 'flex', justifyContent: 'space-between'}">
+      <span>Logo</span>
+      <a-menu
+        v-model:selectedKeys="selectedKeys"
+        mode="horizontal"
+      >
+        <a-menu-item key="1">Products</a-menu-item>
+        <a-menu-item key="2">Stores</a-menu-item>
+        <a-menu-item key="3">Register</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content :style="{ marginTop: '64px', padding: '0px 20px'}">
+      <router-view/>
+    </a-layout-content>
+    <a-layout-footer :style="{ textAlign: 'center' }">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
 
+<script>
+
+export default {
+  name: "App",
+  data(){
+    return {
+      selectedKeys: ['1'],
+    }
+  }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'
+import './assets/antd-custom.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const appInstance = createApp(App);
+appInstance.use(store);
+appInstance.use(router);
+appInstance.use(Antd)
+appInstance.mount("#app");
