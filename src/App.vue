605bbdfd4052ced12f0629a7ccd33141936151b7
felix1251 <domhack123@gmail.com>
2022-11-07 07:18:56 +0800

    vue ant

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,65 @@
 <template>
   <a-layout :style="{height: '100vh'}">
-    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', lineHeight: '64px', display: 'flex', justifyContent: 'space-between'}">
-      <span>Logo</span>
-      <a-menu
-        v-model:selectedKeys="selectedKeys"
-        mode="horizontal"
-      >
-        <a-menu-item key="1">Home</a-menu-item>
-        <a-menu-item key="2">Products</a-menu-item>
-        <a-menu-item key="3">Stores</a-menu-item>
-        <a-menu-item key="4">Register</a-menu-item>
-      </a-menu>
+    <a-layout-header class="header">
+      <div class="d-flex justify-content-between">
+        <span class="fw-bold">Ecommerce Shop</span>
+        <a-menu class="d-none d-sm-block" style="width: 350px" v-model:selectedKeys="selectedKeys" mode="horizontal">
+          <a-menu-item key="1">Home</a-menu-item>
+          <a-menu-item key="2">Products</a-menu-item>
+          <a-menu-item key="3">Stores</a-menu-item>
+          <a-menu-item key="4">Register</a-menu-item>
+        </a-menu>
+        <a-menu class="d-block d-sm-none" style="width: 80px" :trigger="['click']" v-model:selectedKeys="selectedKeys" mode="horizontal">
+          <a-sub-menu>
+            <template #title>
+              <span class="fw-bold">Menu</span>
+            </template>
+            <a-menu-item key="1">Home</a-menu-item>
+            <a-menu-item key="2">Products</a-menu-item>
+            <a-menu-item key="3">Stores</a-menu-item>
+            <a-menu-item key="4">Register</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </div>
     </a-layout-header>
-    <a-layout-content :style="{ marginTop: '64px', padding: '0px 20px', width: '100%'}">
+    <a-layout-content class="content">
       <router-view/>
     </a-layout-content>
-    <a-layout-footer :style="{ textAlign: 'center' }">
-      Ant Design ©2018 Created by Ant UED
+    <a-layout-footer class="footer">
+      Ecommerce Shop ©2022
     </a-layout-footer>
   </a-layout>
 </template>
-
 <script>
-
+import { MenuOutlined } from '@ant-design/icons-vue';
 export default {
   name: "App",
   data(){
     return {
       selectedKeys: ['1'],
+      navWidth: 0,
+      mobileNavWidth: 0,
     }
+  },
+  mounted() {
+
+  },
+  methods: {
+  },
+  components: {
+    MenuOutlined
   }
 }
-
 </script>
 
-<style>
+<style lang="css" scoped>
+  .header{
+    width: 100%;
+  }
+  .content{
+    width: 100%;
+  }
+  .footer{
+    text-align: center;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
+import BootstrapVue3 from 'bootstrap-vue-3'
 import 'ant-design-vue/dist/antd.css'
 import './assets/antd-custom.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const appInstance = createApp(App);
 appInstance.use(store);
+appInstance.use(BootstrapVue3);
 appInstance.use(router);
 appInstance.use(Antd)
 appInstance.mount("#app");
