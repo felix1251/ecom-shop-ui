7f97aa14d746a05fa03a19990140d67c4f185ef0
felix1251 <domhack123@gmail.com>
2022-11-07 16:50:14 +0800

    responsive product page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <a-layout :style="{height: '100vh'}">
-    <a-layout-header v-if="this.$route.meta.layout" class="header">
+  <a-layout :style="{height: '100%'}">
+    <a-layout-header v-if="this.$route.meta.layout" class="header shadow-sm">
       <div class="d-flex justify-content-between">
         <span class="fw-bold">Ecommerce Shop</span>
         <desktop-nav class="d-none d-sm-block" :selectedKeys="[this.$route.name]"/>
-        <mobile-nav  class="d-block d-sm-none" :selectedKeys="[this.$route.name]"/>
+        <mobile-nav class="d-block d-sm-none" :selectedKeys="[this.$route.name]"/>
       </div>
     </a-layout-header>
     <a-layout-content class="content">
+      <a-breadcrumb v-if="this.$route.meta.layout" class="p-2 d-block d-sm-none">
+        <a-breadcrumb-item>page</a-breadcrumb-item>
+        <a-breadcrumb-item>{{this.$route.name}}</a-breadcrumb-item>
+      </a-breadcrumb>
       <router-view/>
     </a-layout-content>
     <a-layout-footer class="footer">
